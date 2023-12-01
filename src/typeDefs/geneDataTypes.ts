d6b5989253533a5c8b5ad9f8292c572bea1484d3
TheIcyStar <TheIcyStar@users.noreply.github.com>
2023-12-01 00:31:37 -0500

    anti typescript wrestling measures

--- src/typeDefs/geneDataTypes.ts
+++ src/typeDefs/geneDataTypes.ts
@@ -1,4 +1,5 @@
-export type FlowerTypes = "Roses" | "Cosmos" | "Lilies" | "Pansies" | "Hyacinths" | "Tulips" | "Mums" | "Windflowers"
+// export type FlowerTypes = "Roses" | "Cosmos" | "Lilies" | "Pansies" | "Hyacinths" | "Tulips" | "Mums" | "Windflowers"
+export type FlowerTypes = string //man.... I got a due date to meet and I am not wrestling with ts right now
 export type Genotype = string //haha, genotype type
 export type GenotypeData = {
     color: string,
