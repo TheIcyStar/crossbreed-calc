8d44e8097052b1ee81d859ec8c157e8f7c6905b8
TheIcyStar <TheIcyStar@users.noreply.github.com>
2023-11-30 20:57:59 -0500

    extra export for genotype data

--- src/typeDefs/geneDataTypes.ts
+++ src/typeDefs/geneDataTypes.ts
@@ -1,16 +1,17 @@
 export type FlowerTypes = "Roses" | "Cosmos" | "Lilies" | "Pansies" | "Hyacinths" | "Tulips" | "Mums" | "Windflowers"
 export type Genotype = string //haha, genotype type
+export type GenotypeData = {
+    color: string,
+    r?: string,
+    y?: string,
+    w?: string,
+    s?: string,
+    oy?: string,
+}
 
 export type GeneDataType = {
     FlowerTypes: {
-        Genotype: {
-            color: string,
-            r?: string,
-            y?: string,
-            w?: string,
-            s?: string,
-            oy?: string,
-        }
+        Genotype: GenotypeData
     }
 }
 
