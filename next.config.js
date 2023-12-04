af7025ae714251f4e1929c7a72bedca2011b578e
TheIcyStar <TheIcyStar@users.noreply.github.com>
2023-12-04 14:32:40 -0500

    gh pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: "http",
@@ -8,7 +9,12 @@ const nextConfig = {
                 pathname: "/**"
             }
         ]
-    }
+    },
+
+    // Used guide: https://github.com/gregrickaby/nextjs-github-pages
+    output: "export",
+    basePath: "/gh-pages",
+
 }
 
 module.exports = nextConfig
